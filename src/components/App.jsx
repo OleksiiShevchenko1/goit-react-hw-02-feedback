c17073f9e9947aea5ecb7aca7b15c13f6b7b8741
OleksiiShevchenko1 <117725999+OleksiiShevchenko1@users.noreply.github.com>
2023-07-06 15:49:02 +0300

    update new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import { Section } from './Section/Section';
 import { React, Component } from 'react';
 import { Statistics } from './Statistics/Statistics';
-import { FeedbackOptions } from './Section/FeedbackOptions/FeedbackOptions';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Notification } from './Notification/Notification ';
 
 export class App extends Component {
   state = {
@@ -15,6 +16,14 @@ export class App extends Component {
     console.log(feedback);
   };
 
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return (100 * (this.state.good / this.countTotalFeedback())).toFixed(0);
+  };
+
   render() {
     return (
       <div>
@@ -25,7 +34,17 @@ export class App extends Component {
           />
         </Section>
         <Section title="Statistics">
-          <Statistics />
+          {this.countTotalFeedback() === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback}
+              positivePercentage={this.countPositiveFeedbackPercentage}
+            />
+          )}
         </Section>
       </div>
     );
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -9,12 +9,14 @@ export const Statistics = ({
 }) => {
   return (
     <div>
-      <h1>{title}</h1>
-      <p>Good:</p>
-      <p>Neutral:</p>
-      <p>Bad:</p>
-      <p>Total:</p>
-      <p>Positive feedback: %</p>
+      <ul>
+        <h1>{title}</h1>
+        <p>Good:{good}</p>
+        <p>Neutral:{neutral}</p>
+        <p>Bad:{bad}</p>
+        <p>Total:{total()}</p>
+        <p>Positive feedback: {positivePercentage()} %</p>
+      </ul>
     </div>
   );
 };
